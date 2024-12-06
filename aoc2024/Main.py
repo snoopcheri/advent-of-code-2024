15b28b74b0199763d4a2befc04b08fd024c26611
Daniel Clausen <dclausen@gradle.com>
2024-12-06 16:19:41 +0100

    Add solution for puzzle 2 part 2

--- aoc2024/Main.py
+++ aoc2024/Main.py
@@ -1,4 +1,4 @@
 from p1 import Puzzle1
 from p2 import Puzzle2
 
-Puzzle2.solve1()
+Puzzle2.solve2()
--- aoc2024/p2/Puzzle2.py
+++ aoc2024/p2/Puzzle2.py
@@ -6,7 +6,6 @@ from aoc2024.common import FileUtils as fu
 
 def solve1():
     reports = fu.read_irregular_matrix("p2/input.txt")
-    print(reports)
 
     # Axis 0 is vertical, axis 1 is horizontal
     result = np.apply_along_axis(is_safe, 1, reports).sum()
@@ -26,3 +25,23 @@ def is_safe(report: ndarray):
         return False
 
     return True
+
+def solve2():
+    reports = fu.read_irregular_matrix("p2/input.txt")
+
+    # Axis 0 is vertical, axis 1 is horizontal
+    result = np.apply_along_axis(is_safe_with_dampener, 1, reports).sum()
+    print(result)
+
+def is_safe_with_dampener(report: ndarray):
+    if is_safe(report):
+        return True
+
+    report = report[~numpy.isnan(report)].astype(int)
+    reports_with_one_missing_level = [np.delete(report, i) for i in range(len(report))]
+
+    for r in reports_with_one_missing_level:
+        if is_safe(r):
+            return True
+
+    return False
