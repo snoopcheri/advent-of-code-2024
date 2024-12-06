9bd68458d66350624237955960409c36ceeaa2cd
Daniel Clausen <dclausen@gradle.com>
2024-12-06 17:42:22 +0100

    Add solution for puzzle 3 part 2

--- aoc2024/Main.py
+++ aoc2024/Main.py
@@ -2,4 +2,4 @@ from p1 import Puzzle1
 from p2 import Puzzle2
 from p3 import Puzzle3
 
-Puzzle3.solve1()
+Puzzle3.solve2()
--- aoc2024/p3/Puzzle3.py
+++ aoc2024/p3/Puzzle3.py
@@ -1,23 +1,21 @@
 from aoc2024.common import FileUtils as fu
 
-import numpy as np
 import re
 
 def solve1():
     lines = fu.read_lines("p3/input.txt")
-
-    result = sum([evalLine(line) for line in lines])
+    result = sum([eval_line(line) for line in lines])
     print(result)
 
 
-def evalLine(line: str) -> int:
+def eval_line(line: str) -> int:
     pattern = re.compile('mul\\(\\d{1,3},\\d{1,3}\\)')
     matches = pattern.findall(line)
 
-    return sum([evalExpr(match) for match in matches])
+    return sum([eval_expr(match) for match in matches])
 
 
-def evalExpr(expr: str) -> int:
+def eval_expr(expr: str) -> int:
     mul_expr = re.compile('mul\\((\\d{1,3}),(\\d{1,3}).*')
     match = mul_expr.match(expr)
 
@@ -25,3 +23,34 @@ def evalExpr(expr: str) -> int:
     b = int(match.group(2))
 
     return a * b
+
+
+def solve2():
+    lines = fu.read_lines("p3/input.txt")
+
+    result = 0
+    do_eval = True
+    for line in lines:
+        val = eval_line_sophisticated(line, do_eval)
+        result += val[0]
+        do_eval = val[1]
+
+    print(result)
+
+def eval_line_sophisticated(line: str, do_eval_initially: bool) -> (int, bool):
+    pattern = re.compile('(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don\'t\\(\\))')
+    matches = pattern.findall(line)
+
+    sum = 0
+    do_eval = do_eval_initially
+
+    for match in matches:
+        if match == "do()":
+            do_eval = True
+        elif match == "don't()":
+            do_eval = False
+        else:
+            if do_eval:
+                sum += eval_expr(match)
+
+    return sum, do_eval
